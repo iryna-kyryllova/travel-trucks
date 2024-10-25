69eb0cf181be78736e96d563bc3c622a16ff1446
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-10-25 14:45:03 +0200

    add routing, pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'))
+const CatalogPage = lazy(() => import('pages/CatalogPage/CatalogPage'))
+const CamperPage = lazy(() => import('pages/CamperPage/CamperPage'))
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'))
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <Suspense fallback='Loading'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/campers' element={<CatalogPage />} />
+        <Route path='/campers/:id' element={<CamperPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
+  )
 }
 
 export default App
