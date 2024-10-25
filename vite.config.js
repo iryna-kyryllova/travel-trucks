a29c4ff0ba684b4b129d0239163aa58d4fd2de96
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-10-25 14:06:47 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true
+  },
+  css: {
+    modules: {
+      generateScopedName: '[name]_[local]_[hash:base64:5]'
+    }
+  },
+  resolve: {
+    alias: {
+      pages: '/src/pages',
+      components: '/src/components',
+      styles: '/src/assets/styles',
+      img: '/src/assets/img',
+      api: '/src/api',
+      utils: '/src/utils'
+    }
+  }
+})
